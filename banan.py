477311e338c2336649405015aedf8e4447684722
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-03 20:37:41 -0600

    game

---
+++ banan.py
@@ -0,0 +1,4 @@
+print("welcome to the game user")
+print("you are probobly wondering where you are. well you are in the grid. the grid is a digital fronteir.")
+print("your goal user is to find and kill clu. also to survive. good luck user you will need it.")
+
