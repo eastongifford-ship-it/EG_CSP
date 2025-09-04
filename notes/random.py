ad7848731b716cec5bb86138f33d0bff1d5c33bd
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-04 14:40:49 -0600

    expression notes

---
+++ notes/random.py
@@ -0,0 +1,29 @@
+# EG 7th expression notes
+
+# what is an algorithm?
+  # set of instructions to complete a task
+name=input("What is your name:\n")
+print("Hello " + name)
+
+# treyson has 12 apples, he has 5 friends that he wants to give apples to. how many apples does each friend get?
+# 2
+apples=12
+friends=5
+print("each friend gets " + str(apples//friends) + " apples")
+
+
+
+
+# list steps in an algorithm
+# 1 declare variables not in computer speed( get needed info)
+# 2 preform any equations
+# 2.5 do the thigys
+# 3 display results
+
+# average age of a group of 4 peeps 
+age1=15
+age2=16
+age3=15
+age4=16
+average=(age1+age2+age3+age4)/4
+print("the average age is " + str(average))
