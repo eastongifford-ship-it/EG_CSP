43c3d88145cf542053b1e691bc74c9d09c7f7670
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-03 20:50:15 -0600

    csp

--- banan.py
+++ banan.py
@@ -1,4 +1,7 @@
 print("welcome to the game user")
 print("you are probobly wondering where you are. well you are in the grid. the grid is a digital fronteir.")
 print("your goal user is to find and kill clu. also to survive. good luck user you will need it.")
-
+print("are you ready user?")
+input("press enter to continue")
+if input("enter") is good
+    print("ok user lets begin")
