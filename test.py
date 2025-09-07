6b3173849525907acdcf511f8e9d285f3a90e7bb
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-07 11:23:07 -0600

    test

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 # EG github connection
-print("Hello, World!")
+user_name= "easton"
+print("hello " + user_name)
+number= 5
+number2= 1000
+food= "eggs"
+color= "blue"
+school= "ucas"
+year= 2025
+age= 15
+subject= "math"
