3cbb4670d1afe8fe10ba126508e5c7b2d90d751d
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-07 12:48:43 -0600

    expressions

---
+++ experession2.py
@@ -0,0 +1,21 @@
+# eg 7th expression practice
+
+expr1 = 7 - 24 / 8 * 4 + 6      # = 1
+expr2 = 18 / 3 - 7 + 2 * 5      # = 9
+expr3 = 6 * 4 / 12 + 72 / 8 - 9 # = 2
+expr4 = (17 - 6 / 2) + 4 * 3    # = 26
+expr5 = -2 * (1 * 4 - 2 / 2) + (6 + 2 - 3)  # = -1
+expr6 = -1 * ((3 - 4 * 7) / 5) - 2 * 24 / 6 # = -3
+expr7 = (3 * 5**2 / 15) - (5 - 2**2)        # = 4
+expr8 = (1**4 * 2**2 + 3**3) - 2**5 / 4     # = 23
+expr9 = (22 / 2 - 2 * 5)**2 + (4 - 6 / 6)**2 # = 10
+
+print(expr1)
+print(expr2)
+print(expr3)
+print(expr4)
+print(expr5)
+print(expr6)
+print(expr7)
+print(expr8)
+print(expr9)
