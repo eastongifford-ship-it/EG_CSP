333121dc97dd03ca1c782a612dc08a31b25d599c
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-09 14:40:54 -0600

    finished notes

--- notes/1/1/strings.py
+++ notes/1/1/strings.py
@@ -9,8 +9,12 @@ full_name = first_name + last_name
 
 
 
-sentence = 'The quick brown fox jumps over the lazy dog'
+sentence = 'The quick brown fox jumps over the lazy dog.          '.strip()
 
+print(sentence.find("jumps"))
+print(sentence[20:25])
+print(sentence[sentence.find("lazy"): len("lazy")+sentence.find("lazy")]) # this is slicing 
+print(len(first_name)) # finds length of string
 print('welcome to my program', first_name, last_name +"!")
 
 
@@ -18,5 +22,19 @@ print('welcome to my program', first_name, last_name +"!")
 
 #concatanate is joining two or more strings together
 #sanitization and stupid profing
+#stupid profing is making sure that the user doesnt break your code
+#sanitization is step one of stupid profing
+#debugging takes forever
+# bug is an error in your code
+# there are 3 types of bugs 
+# 1. syntaxt error is a mistake in your grammer.
+# couses of syntax error are spaces missplelling forgeting a comma or =.
+# 2. logic error - 1 + 2 = 12 # its a mistake in your logic
+# 3. runtime error - when your code looks fine but when you run it breaks
+# it runs until it hits the error
+# index counts starting from 0
+
+
+
 
 #debugging is fixing code
