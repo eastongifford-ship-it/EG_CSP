094b21625ce9bfef36e1cb8b79265f77f3afe5a1
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-10 14:27:06 -0600

    silly sentances

---
+++ notes/practices/silly-sentences.py
@@ -0,0 +1,6 @@
+# EG 7th silly sentences
+
+color = input("name a color: \n")
+name = input("name a person: \n")
+animal = input("name a animal: \n")
+print("Roses are " + color + " " + "violets are blue." + " " + name + " " + "looks like a" + " " + animal + " " + "but looks better than you.")
