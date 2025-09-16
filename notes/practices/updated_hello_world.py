e838a715204bc3dc6a238aa0c478c84e308674d7
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-16 14:34:14 -0600

    homework

---
+++ notes/practices/updated_hello_world.py
@@ -0,0 +1,12 @@
+# EG 7th updated hello world
+
+def hello(name):
+    print(f"Hello {name}")
+    
+name=input("what is your name\n")
+hello('bob')
+hello('bobby')
+hello('john')
+hello('johnny')
+hello("fred")
+
