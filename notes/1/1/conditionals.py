790700da19009f30e1705cec1befa537bdcc0e19
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-17 14:41:49 -0600

    fix

---
+++ notes/1/1/conditionals.py
@@ -0,0 +1,12 @@
+#eg 7th conditional notes
+# if = check true or false
+# boolean is true or false
+# one = messes up the varilbe two == is a ?
+# == is are they equal
+#homework_done = input('did you do your homework?\n').strip().capitalize()
+
+#if homework_done == "yes":
+    #print('yes you can go')
+#else:
+    #print("then go do your homework!")
+grade = 
