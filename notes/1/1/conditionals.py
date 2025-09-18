20dd18a74a1c223389e87de24446d31136cf8562
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-18 14:00:14 -0600

    idk

--- notes/1/1/conditionals.py
+++ notes/1/1/conditionals.py
@@ -9,4 +9,12 @@
     #print('yes you can go')
 #else:
     #print("then go do your homework!")
-grade = 
+#grade = 89.5
+
+name = input(' What is your name :\n')
+if name == "ms Larose":
+    print("you are the teacher: \n")
+elif name== "tia":
+    print('you are a ta')
+else: 
+    print('you are a student')
