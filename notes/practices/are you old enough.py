f39b0ae6eddc0092aa1ff8fa2f23f3bdb91a7b74
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-18 14:31:13 -0600

    homwork

---
+++ notes/practices/are you old enough.py
@@ -0,0 +1,14 @@
+#EG 7th old enough
+
+age = int(input("how old are you"))
+
+if age >= 4 and age <= 14:
+    print("you can go to school")
+elif age == 15:
+    print("you can have a permit")
+elif age <= 17 and age >= 16:
+    print('you can have a licence ')
+elif age <=3:
+    print('no your to young')
+else:
+    print("you can vote")
