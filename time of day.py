981b43b40b1bb2d17177e6f78ee7b206265e5f1a
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-18 20:31:32 -0600

    time of day homwwork

--- time of day.py
+++ time of day.py
@@ -1,2 +1,11 @@
 # EG 7th time of day
 
+time = int(input("what time is it"))
+if time >=17 and time <= 24:
+    print("good evening")
+elif time <= 11 and time > 0:
+    print("good morning")
+elif time >= 12 and time <=16:
+    print('good afternoon')
+else:
+    print('please enter a valid time')
