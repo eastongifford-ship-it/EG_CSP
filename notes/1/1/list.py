fbb58289f21706b2c1efbe689703aca3918eadb2
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-22 14:27:01 -0600

    loops

---
+++ notes/1/1/list.py
@@ -0,0 +1,7 @@
+# eg 7th  list notes
+
+siblings = ["chandler", "Brooklyn", "ivy"]
+
+print(siblings[0])
+print(siblings[1])
+print(siblings[2])
---
+++ notes/1/1/loops.py
@@ -0,0 +1,26 @@
+# EG 7th loops
+
+foods = ["Banana", "Pasta", "Bread", "apple", 'steak']
+
+# for loop loops repeat code inside them for a specific number of time
+#loops repeat code inside them for a specific number of time
+for food in foods:
+    #print(f"{food} is my favorite food!")
+    print("hi!")
+
+for x in range (1,21):
+    print(x)
+
+# while loops breaks stuff apart
+# infinit loop goes on for ever
+ 
+#while True:
+    #print(i)
+    #i+= 0
+    #infinent loop 
+
+    #corect while loop
+i = 1
+while 1 <= 20:
+    print(i)
+    i+= 1
