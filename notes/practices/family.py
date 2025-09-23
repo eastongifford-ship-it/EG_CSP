30e0db86e5bb3e16bfa2361f571ab63c351acf23
eastongifford-ship-it <easton.gifford@ucas-edu.net>
2025-09-23 14:37:54 -0600

    work

---
+++ notes/practices/family.py
@@ -0,0 +1,6 @@
+# Eg 7th family loops
+
+family_members = ("ivy", "chandler", "mom", "dad")
+
+for family_member in family_members:
+    print(f'Hi {family_member}')
---
+++ notes/practices/fizzbuzz.py
@@ -0,0 +1,18 @@
+# EG 7th fizz buzz
+
+x = 1
+
+
+
+for x in range(1, 50): 
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzbuzz")
+    elif x % 3 == 0:
+        print("fizz")
+    elif x % 5 == 0:
+        print("buzz")
+    else:
+        print(x)
+    
+    
+    x+= 1 
