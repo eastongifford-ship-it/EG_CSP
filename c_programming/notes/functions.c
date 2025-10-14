ba6be5db5196ce1c54bc1e5aed736430b8eeb1e2
easton.gifford-ship-it <easton.gifford@ucas-edu.net>
2025-10-14 14:42:09 -0600

    67676767676

---
+++ c_programming/notes/functions.c
@@ -0,0 +1,31 @@
+// Eg 7th function notes
+
+#include <stdio.h>
+#include <string.h>
+
+
+
+void birthday(char*name){
+    printf("happy birthday to you\n");
+    printf("happy birthday to you\n");
+    printf("happy birthday dear %s\n",name);
+    printf("happy birthday to you\n");
+    printf("happy birthday %s your 67 years old\n",name);
+}
+
+
+
+int main(void){
+    birthday("bob");
+    birthday("bobby");
+    birthday("john");
+    
+
+
+
+
+
+
+
+    return 0;
+}
