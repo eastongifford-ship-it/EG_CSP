978c4e677cdd1ca04138d21522711ede3f89e417
easton.gifford-ship-it <easton.gifford@ucas-edu.net>
2025-10-24 14:42:40 -0600

    67

--- c_programming/notes/sandbox2.c
+++ c_programming/notes/sandbox2.c
@@ -4,42 +4,36 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void){
-   
-    
-    int life(void){
-        int lives = 6;
-        if(lives == 6){
+
+
+
+
+int life(void){
+    int lives = 5;
+    if(lives == 6){
         printf("_____\n|    |\n|    |\n|    \n|    \n|_______\n");
-        }else if(lives == 5){
+    }else if(lives == 5){
         printf("_____\n|    |\n|    |\n|    o\n|    \n|_______\n");
-        }else if(lives == 4){
+    }else if(lives == 4){
         printf("_____\n|    |\n|    |\n|    o\n|   / \n|_______\n");
-        }else if(lives == 3){
+    }else if(lives == 3){
         printf("_____\n|    |\n|    |\n|    o\n|   /| \n|_______\n");
-        }else if(lives == 2){
+    }else if(lives == 2){
         printf("_____\n|    |\n|    |\n|    o\n|   /|\\ \n|_______\n");
-        }else if(lives == 1){
+    }else if(lives == 1){
         printf("_____\n|    |\n|    |\n|    o\n|   /|\\ \n|   /  \n|_______\n");
-        }else{
+    }else{
         printf("_____\n|    |\n|    |\n|    o\n|   /|\\ \n|   / \\ \n|_______\n");
-        }
-        
-    };
-    life();
+    }
     
     return 0;
 }
 
 
+int main(void){
+    
+    life();
+    return 0;
 
-void choose_random_word() {
-    srand(time(NULL));
-	char words[60][30] = {"potato", "wagon", "cooperate", "seek", "sulphur", "center", "bounce", "ruin", "interference", "autonomy", "examination", "roof", "market", "pop", "light", "shock", "skin", "donor", "element", "execution", "vessel", "contrast", "coding", "python", "boring", "octopus"};
-	int num = rand() % 8;
-    char word[30] = words[num];
-    char display[30];
-    for(int i = 0; i < strlen(word); i++) {
-        display[i] = '_';
-    }
 }
+
